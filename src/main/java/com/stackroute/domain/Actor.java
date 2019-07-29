08b79012c1e568965cd8030976576de9544ae112
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-29 13:38:05 +0530

    pushing to aware-interface

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -3,9 +3,9 @@ package com.stackroute.domain;
 
 public class Actor {
 
-     String name;
-     String gender;
-     int age;
+    String name;
+    String gender;
+    int age;
 
     public Actor(String name, String gender, int age) {
         this.name = name;
