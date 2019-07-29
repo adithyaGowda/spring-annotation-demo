d0d71fa2171aee044a5749d7c6c790324c15f097
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-29 20:26:12 +0530

    adding files to bean-post-processor repo

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,35 +2,36 @@ package com.stackroute;
 
 
 import com.stackroute.demo.BeanLifecycleDemoBean;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import org.springframework.context.support.AbstractApplicationContext;
 
 
 public class Main
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        Actor actorRef1 = context.getBean("actor1", Actor.class);
-        Actor actorRef2 = context.getBean("actor2", Actor.class);
-        Actor actorRef3 = context.getBean("actor3", Actor.class);
-
-        System.out.println(actorRef1.toString());
-        System.out.println(actorRef2.toString());
-        System.out.println(actorRef3.toString());
-
-        Movie movieRef1 = context.getBean("movie1",Movie.class);
-
-
-        System.out.println(movieRef1.toString());
-        
-
-
+//        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//
+//        Actor actorRef1 = context.getBean("actor1", Actor.class);
+//        Actor actorRef2 = context.getBean("actor2", Actor.class);
+//        Actor actorRef3 = context.getBean("actor3", Actor.class);
+//
+//        System.out.println(actorRef1.toString());
+//        System.out.println(actorRef2.toString());
+//        System.out.println(actorRef3.toString());
+//
+//        Movie movieRef1 = context.getBean("movie1",Movie.class);
+//
+//
+//        System.out.println(movieRef1.toString());
+//
+//
+//
         System.out.println("-----------------------------------------------------------------------------------------------------");
 
 
@@ -41,6 +42,16 @@ public class Main
 
         applicationContext.close();
 
+        System.out.println("-----------------------------------------------------------------------------------------------------");
+
+
+        AbstractApplicationContext abstractApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        BeanPostProcessorDemoBean bean = abstractApplicationContext.getBean("beanPostProcessorDemoBean",BeanPostProcessorDemoBean.class);
+
+        System.out.println( bean.getMessage());
+
+        abstractApplicationContext.registerShutdownHook();
 
 
     }
